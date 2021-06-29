96194b796fe05d233d32cde342b5a0b0860b3e2b
Oustinger <oustinger@gmail.com>
2021-06-29 17:05:11 +0500

    Add project scheme (.drawio)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
@@ -10,19 +10,17 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 const App = (props) => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header />
-                <Navbar />
-                <div className="app-wrapper_content">
-                    <Route path="/dialogs" render={() => <DialogsContainer />} />
-                    <Route path="/profile" render={() => <Profile />} />
-                    <Route path="/news" render={() => <News />} />
-                    <Route path="/music" render={() => <Music />} />
-                    <Route path="/settings" render={() => <Settings />} />
-                </div>
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <div className="app-wrapper_content">
+                <Route path="/dialogs" render={() => <DialogsContainer />} />
+                <Route path="/profile" render={() => <Profile />} />
+                <Route path="/news" render={() => <News />} />
+                <Route path="/music" render={() => <Music />} />
+                <Route path="/settings" render={() => <Settings />} />
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img className={s.logo} src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" />
+            <img className={s.logo} src="https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png" />
         </header>
     );
 }
