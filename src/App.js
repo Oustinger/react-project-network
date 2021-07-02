8c2f1dbdd773e09869358166fc6e86bd66a19de5
Oustinger <oustinger@gmail.com>
2021-07-02 10:03:35 +0500

    lesson 51

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import UsersContainer from './components/Users/UsersContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
@@ -16,6 +17,7 @@ const App = (props) => {
             <div className="app-wrapper_content">
                 <Route path="/dialogs" render={() => <DialogsContainer />} />
                 <Route path="/profile" render={() => <Profile />} />
+                <Route path="/users" render={() => <UsersContainer />} />
                 <Route path="/news" render={() => <News />} />
                 <Route path="/music" render={() => <Music />} />
                 <Route path="/settings" render={() => <Settings />} />
